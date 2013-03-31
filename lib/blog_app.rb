10bb7c4c848c8c0cea189414a78a9c8bad5b79ea
Lenon Marcel <lenon.marcel@gmail.com>
2013-03-31 20:36:57 -0300

    Use double quotes instead of single quotes for better readability (BlogApp)

--- lib/blog_app.rb
+++ lib/blog_app.rb
@@ -1,11 +1,11 @@
 class BlogApp < App
-  get '/' do
+  get "/" do
     @posts = posts_scope.desc(:created_at)
     erb :index
   end
 
-  get '/post/:slug' do
-    @post = posts_scope.find(params[:slug]) || not_found
+  get "/post/:slug" do
+    @post  = posts_scope.find(params[:slug]) || not_found
     @title = @post.title
     erb :post
   end
