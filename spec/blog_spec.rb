05dba17c071c13972e8935bbefdd794d67e89115
Lenon Marcel <lenon.marcel@gmail.com>
2013-03-31 20:30:12 -0300

    Refactor blog_spec for better readability

--- spec/blog_spec.rb
+++ spec/blog_spec.rb
@@ -8,43 +8,56 @@ describe "BlogApp" do
   let!(:third_post) { create :post, :domain => "foobar.com" }
 
   describe "GET /" do
-    specify "default domain will be used to filter posts" do
-      get "/"
+    context "HTTP_HOST is empty" do
+      it "list all posts using the default domain as filter" do
+        get "/"
 
-      last_response.should be_ok
-      last_response.body.should include first_post.title
-      last_response.body.should include second_post.title
-      last_response.body.should_not include third_post.title
+        last_response.should be_ok
+        last_response.body.should include first_post.title
+        last_response.body.should include second_post.title
+        last_response.body.should_not include third_post.title
+      end
     end
 
-    specify "given domain will be used to filter posts" do
-      get "/", {}, "HTTP_HOST" => "foobar.com"
+    context "HTTP_POST is set" do
+      it "list all posts using HTTP_HOST as domain filter" do
+        get "/", {}, "HTTP_HOST" => "foobar.com"
 
-      last_response.should be_ok
-      last_response.body.should_not include first_post.title
-      last_response.body.should_not include second_post.title
-      last_response.body.should include third_post.title
+        last_response.should be_ok
+        last_response.body.should_not include first_post.title
+        last_response.body.should_not include second_post.title
+        last_response.body.should include third_post.title
+      end
     end
   end
 
   describe "GET /post/:slug" do
-    it "should show the post" do
-      get "/post/#{first_post.slug}"
+    context "HTTP_HOST is empty" do
+      it "show a post associated with default domain" do
+        get "/post/#{first_post.slug}"
 
-      last_response.should be_ok
-      last_response.body.should include first_post.title
-      last_response.body.should_not include second_post.title
+        last_response.should be_ok
+        last_response.body.should include first_post.title
+        last_response.body.should_not include second_post.title
+      end
+
+      it "returns 404 for a post associated with another domain" do
+        get "/post/#{first_post.slug}", {}, "HTTP_HOST" => "foobar.com"
+        last_response.status.should be 404
+      end
     end
 
-    it "should return HTTP 404 when post is not found" do
+    context "HTTP_HOST is set" do
+      it "show a post associated with HTTP_HOST" do
+        get "/post/#{third_post.slug}", {}, "HTTP_HOST" => "foobar.com"
+
+        last_response.should be_ok
+        last_response.body.should include third_post.title
+      end
+    end
+
+    it "returns 404 when post is not found" do
       get "/post/post-que-no-ecziste"
-
-      last_response.status.should == 404
-    end
-
-    specify "domain will be used to filter a post" do
-      get "/post/#{first_post.slug}", {}, "HTTP_HOST" => "foobar.com"
-
       last_response.status.should == 404
     end
   end
